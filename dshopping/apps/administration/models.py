b130f67c8a011c373097fd63aaedf92d1c1d5bb8
juanfebon <juanfebon995@gmail.com>
2019-10-23 10:34:01 -0500

    DSHOPPING_FINAL

--- dshopping/apps/administration/models.py
+++ dshopping/apps/administration/models.py
@@ -3,31 +3,30 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
-    id = models.AutoField(primary_key=True)
+    
     title = models.CharField('Name', max_length=150, unique=True)
     description = models.TextField('Description')
     status = models.BooleanField('State', default=True)
     class Meta:
-        verbose_category = 'Category'
-        verbose_category_plural = 'Categories'
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
     
     def __str__(self):
         return self.title
 
 class Country(models.Model):
-    id = models.AutoField(primary_key=True)
+
     title = models.CharField('Name', max_length=150, unique=True)
     abbreviation = models.TextField('Abbreviation')
     status = models.BooleanField('State', default=True)
     class Meta:
-        verbose_Country = 'Country'
-        verbose_Countries_plural = 'Countries'
+        verbose_name = 'Country'
+        verbose_name_plural = 'Countries'
     
     def __str__(self):
         return self.abbreviation
 
-class Product(Category,Country):
-    id = models.AutoField(primary_key=True)
+class Product(models.Model):
     title = models.CharField('Name', max_length=150, unique=True)
     description = models.TextField('Description')
     photo = models.ImageField('Image', upload_to= 'images/',max_length=200)
@@ -36,26 +35,26 @@ class Product(Category,Country):
     quantity = models.IntegerField('Quantity', max_length=10)
     status = models.BooleanField('State', default=True)
     class Meta:
-        verbose_product = 'Product'
-        verbose_products_plural = 'Products'
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
     
     def __str__(self):
         return self.title
 
 class Gender(models.Model):
-    id = models.AutoField(primary_key=True)
+
     title = models.CharField('Name', max_length=150, unique=True)
     description = models.TextField('Description')
     status = models.BooleanField('State', default=True)
     class Meta:
-        verbose_gender = 'Gender'
-        verbose_gender_plural = 'Genders'
+        verbose_name = 'Gender'
+        verbose_name_plural = 'Genders'
     
     def __str__(self):
         return self.title
 
-class Client(Gender,Country):
-    id = models.AutoField(primary_key=True)
+class Client(models.Model):
+    
     firstname = models.CharField('First Name', max_length=150)
     lastname = models.CharField('LastName', max_length=150)
     id_gender = models.ForeignKey(Gender, on_delete=models.CASCADE)
@@ -66,17 +65,19 @@ class Client(Gender,Country):
     photo = models.ImageField('Image', upload_to= 'images/',max_length=200)
     credit_card_number = models.CharField('Phone', max_length=20)
     status = models.BooleanField('State', default=True)
+    
     class Meta:
-        verbose_Client = 'Client'
-        verbose_Clients_plural = 'Clients'
+        verbose_name = 'Client'
+        verbose_name_plural = 'Clients'
     
     def __str__(self):
-        return self.'{0}{1}{2}'.format(self.lastname, ", ", self.firstname)
+        return '{0}{1}{2}'.format(self.lastname,',', self.firstname)
 
-class shopping(Product,Client):
-    id = models.AutoField(primary_key=True)
+class shopping(models.Model):
+    
     id_product = models.ForeignKey(Product, on_delete=models.CASCADE)
     id_client = models.ForeignKey(Client, on_delete=models.CASCADE)
     shopping_date = models.DateField('Shopping date', auto_now=False, auto_now_add=True)
-    class Meta:
-        abstract = True
+
+    def __int__(self):
+        return self.shopping_date
